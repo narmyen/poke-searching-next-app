bcfc4b4f70c57d9a7bab4d0044b57918a632a420
narmyen <tipananchakreerat@gmail.com>
2024-04-07 14:55:21 +0700

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Header from "./components/Header";
+import PokeData from "./components/PokeData";
+
+export default function Home() {
+  return (
+    <> 
+      <Header></Header>
+      <PokeData/>
+    </>
+  );
+}
